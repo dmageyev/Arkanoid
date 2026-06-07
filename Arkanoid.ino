289243ce777acc6aa3ec2620bec4bf0b595a02be
Dmytro Ageyev <dmytro.aheiev@nure.ua>
2026-06-07 10:20:38 +0300

    Implement level creation and brick drawing
    Added level creation and brick drawing functionality.

--- Arkanoid.ino
+++ Arkanoid.ino
@@ -1,13 +1,6 @@
 #include <GameBoy.h>
+#include "Blocks.h"
 GameBoy gb;
-
-int paddleX = 3;
-int paddleY = 13;
-
-int ballX = 4;
-int ballY = 13;
-int directionX = 1;
-int directionY = -1;
 
 byte paddle[3] = { 1, 1, 1 };
 
@@ -24,19 +17,35 @@ byte Block_level_2[4][8] = {
   { 1, 0, 1, 0, 1, 0, 1, 0 }
 };
 
+int paddleX = 3;
+int paddleY = 14;
+
+int ballX = 4;
+int ballY = 13;
+int directionX = 1;
+int directionY = -1;
+
+bool stateLevel = true;
+int numLevel = 1;
+
+
 
 void setup() {
   gb.begin(0);
-  randomSeed(analogRead(0) + analogRead(5));
+  createLevel();
+  Serial.begin(9600);
 }
 
 void loop() {
+
+
   ball();
   makePaddle();
   drawPaddle(paddle, paddleX, paddleY);
   delay(500);
   gb.drawDisplay();
 }
+
 
 void drawPaddle(byte arr[3], int x, int y) {
   for (int i = 0; i < 3; i++) {
@@ -70,4 +79,49 @@ void checkCollision() {
   if (ballY == paddleY - 1 && ballX >= paddleX && ballX <= paddleX + 3 ) {
     directionY = -1;
   }
+
+  if (gb.checkCollision(ballX, ballY)) {
+    gb.wipePoint(ballX, ballY);
+    directionY = 1;
+    gb.sound(SCORE);
+
+  }
 }
+
+
+void drawBricks(byte arr[4][8]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (arr[i][j] == 1) {
+        gb.memDisplay(j, i);
+
+      }
+    }
+  }
+}
+
+
+void createLevel() {
+  memClear();
+  if (numLevel == 1 && stateLevel) {
+    gb.clearDisplay();
+    drawBricks(Block_level_1);
+    stateLevel = false;
+  }
+  else if (numLevel == 2 && stateLevel) {
+    gb.clearDisplay();
+    drawBricks(Block_level_2);
+    stateLevel = false;
+  }
+}
+
+void memClear() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.display[j][i] = 0;
+    }
+  }
+}
+
+
+
